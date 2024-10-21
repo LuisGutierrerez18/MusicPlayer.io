17dd6cc20355c521ade5596f4a494a34ca746a83
Luis Gutierrez <A00837481@tec.mx>
2024-10-21 13:55:24 -0600

    Button + image change

--- Tarea 3/canciones.js
+++ Tarea 3/canciones.js
@@ -1,10 +1,12 @@
-
-const imagenes = ["images/Shivers.png","images/Photograph.png","images/ShapeOfYou.png"]
-const names = ["Shivers","Photograph","Shape of You"]
-let index = 0; 
-
+let index = 0;
+const imagenes = ["images/Shivers.png","images/Photograph.png","images/ShapeofYou.png"]
 function cambiarImagen(){
-    let img = document.getElementById('imagen')
-    let name = document.getElementById('nombreCancion')
-    
+        // Obtener la imagen actual del carrusel
+        let img = document.getElementById('imagenCarrusel');
+        
+        // Cambiar al siguiente index
+        index = (index + 1) % imagenes.length;
+        
+        // Cambiar la fuente de la imagen
+        img.src = imagenes[index];
 }

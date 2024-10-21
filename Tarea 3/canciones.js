5f7242037be425e739b55cd024aa9343bc6117ed
Luis Gutierrez <A00837481@tec.mx>
2024-10-21 14:36:39 -0600

    Centered Button

--- Tarea 3/canciones.js
+++ Tarea 3/canciones.js
@@ -1,5 +1,6 @@
 let index = 0;
 const imagenes = ["images/Shivers.png","images/Photograph.png","images/ShapeofYou.png"]
+const nombres = ["Shivers","Photograph","Shape of You"]
 function cambiarImagen(){
         // Obtener la imagen actual del carrusel
         let img = document.getElementById('imagenCarrusel');

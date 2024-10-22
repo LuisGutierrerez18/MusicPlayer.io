57d6fcfefbe730841023f0bbc70f1d93e8dcbe78
Luis Gutierrez <A00837481@tec.mx>
2024-10-22 14:02:31 -0600

    Prev Button working + More Songs

--- Tarea 3/canciones.js
+++ Tarea 3/canciones.js
@@ -1,7 +1,9 @@
 let index = 0;
-const imagenes = ["images/Shivers.png","images/Photograph.png","images/ShapeofYou.png"]
-const names = ["Shivers", "Photograph", "Shape of You"]
-function cambiarImagen(){
+const imagenes = ["images/Shivers.png","images/Photograph.png","images/ShapeofYou.png","images/ThinkingOutLoud.png",
+                  "images/Perfect.png", "images/CastleOnTheHill.png"
+                ]
+const names = ["Shivers", "Photograph", "Shape of You", "Thinking Out Loud", "Perfect", "Castle On The Hill"]
+function nextSong(){
         // Obtener la imagen actual del carrusel
         let img = document.getElementById('imagenCarrusel');
         let nombre = document.getElementById('nombreCancion')
@@ -13,5 +15,18 @@ function cambiarImagen(){
         nombre.innerText = names[index];
         nombre.style.opacity = "1"; 
         nombre.style.visibility = "visible"; 
+}
 
+function previousSong(){
+        // Obtener la imagen actual del carrusel
+        let img = document.getElementById('imagenCarrusel');
+        let nombre = document.getElementById('nombreCancion')
+        // Cambiar al siguiente index
+        index = (index - 1+imagenes.length) % imagenes.length;
+        
+        // Actualiza la imagen y nombre
+        img.src = imagenes[index];
+        nombre.innerText = names[index];
+        nombre.style.opacity = "1"; 
+        nombre.style.visibility = "visible"; 
 }
